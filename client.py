b234add4e54b2de9be318686879c5ef2dd1872f1
Sarthak Shukla <sarthakshukla.eddy@gmail.com>
2020-08-07 01:08:53 +0530

    Modify client and server files with register, search and listing files options

--- client.py
+++ client.py
@@ -1,7 +1,82 @@
+import os
+import pickle
+import threading
 import socket
+from threading import *
 
-client = socket.socket()
 
-client.connect(('localhost', 3456))
+class peerServer:
+    def __init__(self):
+        print("Welcome to peer to peer file sharing system")
+        self.file_name = ""
+        self.peer_port = 0
 
-print(client.recv(1024).decode())
+    def start_server(self):
+        while True:
+            choice = input("Menu \n(1) Register\n(2) Search\n(3) List all files\n(4) Download\n(5) Exit server\n")
+            choice = int(choice)
+            if choice == 1:
+                peer_id = input("Enter the 4 digit peer id: ")
+                self.file_name = input("Enter file name: ")
+                self.peer_port = int(peer_id)
+                self.register()
+                # start_listener(self.peer_port, 'loaclhost')
+
+            elif choice == 2:
+                self.search()
+
+            elif choice == 3:
+                self.list_all()
+
+            elif choice == 4:
+                peer_id = input("Enter the 4 digit peer id: ")
+                file_name = input("Enter file name: ")
+                self.download(int(peer_id), file_name)
+
+            elif choice == 5:
+                break
+
+            else:
+                continue
+
+    def register(self):
+        client = socket.socket()
+        client.connect(('localhost', 3456))
+        register_data = [1, self.peer_port, self.file_name]
+        data = pickle.dumps(register_data)
+        client.send(data)
+        state = client.recv(1024)
+        print(state.decode('utf-8'))
+        client.close()
+
+    def search(self):
+        client = socket.socket()
+        client.connect(('localhost', 3456))
+        file_name = input("Enter file name: ")
+        register_data = [2, file_name]
+        data = pickle.dumps(register_data)
+        client.send(data)
+        state = pickle.loads(client.recv(1024))
+        self.print_list(state[0], state[1])
+        client.close()
+
+    def list_all(self):
+        client = socket.socket()
+        client.connect(('localhost', 3456))
+        data = pickle.dumps(str(3))
+        client.send(data)
+        state = pickle.loads(client.recv(1024))
+        self.print_list(state[0], state[1])
+        client.close()
+
+    def print_list(self, files, keys):
+        if len(files) == 0:
+            print("There is no file available")
+        else:
+            print("Peer ID  File Name    Date Added")
+            for item in files:
+                print(item[keys[0]],"  ",item[keys[1]], "    ", item[keys[2]])
+
+
+peer = peerServer()
+peer.start_server()
--- server.py
+++ server.py
@@ -1,16 +1,73 @@
 import socket
+import threading
+import pickle as pk
+from datetime import datetime
+from threading import *
 
-serv = socket.socket()
-print("Socket Created")
 
-serv.bind(('localhost', 3456))
+class mainServer(threading.Thread):
+    def __init__(self, max_connection):
+        threading.Thread.__init__(self)
+        self.host = 'localhost'
+        self.semaphore = Semaphore(max_connection)
+        self.port = 3456
+        self.serv = socket.socket()
+        print("Central server is live")
+        self.serv.bind((self.host, self.port))
+        self.serv.listen(max_connection)
+        self.files = []
+        self.keys = ['peer_id', 'file_name', 'date_added']
+        print("Get connected at", self.host, "on port number", self.port)
 
-serv.listen(1)
-print("Waiting for connection")
+    def run(self):
+        while True:
+            client, addr = self.serv.accept()
+            print("Connected to", addr[0], ". Port at", addr[1])
 
-while True:
-    client, addr = serv.accept()
-    print("Connected with", addr)
+            request = pk.loads(client.recv(1024))
 
-    client.send(bytes('Welcome to my server', 'utf-8'))
-    client.close()
+            if request[0] == 1:
+                print("Peer", addr[1], "Add new file")
+                self.semaphore.acquire()
+                self.register(request[1], request[2], str(datetime.now()))
+                ret = "File Registered Successfully."
+                client.send(bytes(ret, 'utf-8'))
+                self.semaphore.release()
+                client.close()
+            elif request[0] == 2:
+                print("Peer", addr[1], "Searching for a file")
+                self.semaphore.acquire()
+                ret_data = pk.dumps(self.search_data(request[1]))
+                client.send(ret_data)
+                self.semaphore.release()
+                client.close()
+            elif request[0] == 3:
+                print("Peer", addr[1], "Listing all files")
+                self.semaphore.acquire()
+                ret_data = pk.dumps(self.all_data())
+                client.send(ret_data)
+                self.semaphore.release()
+                client.close()
+            else:
+                continue
+
+    def register(self, peer_id, file_name, date):
+        entry = [str(peer_id), file_name, str(date)]
+        self.files.insert(0, dict(zip(self.keys, entry)))
+
+    def search_data(self, file_name):
+        ret = []
+        for item in self.files:
+            if item['file_name'] == file_name:
+                entry = [item['peer_id'], item['file_name'], item['date_added']]
+                ret.insert(0, dict(zip(self.keys, entry)))
+        return ret, self.keys
+
+    def all_data(self):
+        return self.files, self.keys
+
+
+# Main
+print("Welcome. Server is about to go live")
+server = mainServer(2)
+server.start()
